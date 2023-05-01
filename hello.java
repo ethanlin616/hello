ae4914344d3c02ff9cb629c06e94e9f3d19da4d2
ethanlin <lzh_working@126.com>
2023-05-01 13:37:47 +0800

    second

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@ public class hello {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Hello, World!");
         System.out.println("I am ethan lin");
+        System.out.println("second update");
 
     }
 }
