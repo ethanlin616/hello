a2687968f7f0ee553d245e8f35ab682782114fdd
ethanlin <lzh_working@126.com>
2023-05-01 12:47:11 +0800

    first

---
+++ hello.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, Hello, World!");
+        System.out.println("I am ethan lin");
+
+    }
+}
